bc5977071774f620007f6aa2f773cd3c9b2ddae4
jay-aree <jayyqwe123@gmail.com>
2022-08-15 10:49:47 +0700

    Generate images from API

--- realestate/components/Property.jsx
+++ realestate/components/Property.jsx
@@ -6,9 +6,16 @@ import {BsGridFill} from 'react-icons/bs';
 import {GoVerified} from 'react-icons/go';
 import millify from 'millify';
 
+import DefaultImage from '../assets/images/house.jpg';
+
 const Property = ({property :{ coverPhoto, price, rentFrequency,title,baths,area, agency, isVerified,externalId}}) => (
     <Link href={`/property/${externalId}`} passHref>
-        {title}
+        <Flex flexWrap = "wrap " w ="420px" p= "5" paddingTop="0"   justifyContent="flex-start" cursor="pointer">
+            <Box>
+                <Image src = {coverPhoto ? coverPhoto.url : DefaultImage} width={400} height = {360} alt = "house" />
+            </Box>
+        </Flex>
+    
     </Link>
 
 );
