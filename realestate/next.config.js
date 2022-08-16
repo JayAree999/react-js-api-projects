32f76f29f4c0a6a6f4977c4e4747cd3f467a3496
jay-aree <jayyqwe123@gmail.com>
2022-08-16 17:52:54 +0700

    Update next.config.js

--- realestate/next.config.js
+++ realestate/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains : ['bayut-production.s3.eu-central-1.amazonaws.com']
   }
